422f4035f81736be68152416bd7b5df2e87fa335
valdeirgsilva <valdeirgabriels@gmail.com>
2022-11-16 22:04:38 -0300

    Criando mecanismo para limitar na primeira geração de Pokémon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
-const limit = 5;
+
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function loadPokemonItens(offset, limit) {
@@ -29,5 +31,15 @@ loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit;
-    loadPokemonItens(offset, limit);
+
+    const qtdRecordsWithNextPage = offset + limit;
+
+    if (qtdRecordsWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemonItens(offset, limit);
+    }
 });
